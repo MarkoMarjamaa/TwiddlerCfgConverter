9d29e8b1d565709c830d0cf1abd9e9324add727f
MarkoMarjamaa <marko.marjamaa@iki.fi>
2015-05-12 17:29:03 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # TwiddlerCfgConverter
 Converts Twiddler3 keyboard configuration files to human readable format and vice versa
+
+Faster to create new configurations( from existing ones)
+Allows using international charactersets and keyboards
+Allows writing comments about design decisions. 
+Allows making groups of similar chord combinations for easier learning
+
+Convert Twiddler3 cfg to text file 
+powershell -file TwiddlerCfg2Text.ps1 inputfilename outputfilename [HID file name]
+
+Examples:
+powershell -file TwiddlerCfg2Text.ps1 twiddler_default.cfg twiddler_default.text.cfg
+powershell -file TwiddlerCfg2Text.ps1 twiddler_default.cfg twiddler_default.text.cfg Us.hid.txt
+
+Text file format: 
+
+# Starts a comment
+
+Single key: 
+modifier chords hid_modifier&key character
+
+Example:
+  AN LLOL 0047 <ScrollLock>
+   O ROOO 0004 a
+
+Macro keys: 
+modifier chords hid_modifier&key,hid_modifier&key2... character1character2
+
+Example:
+   O OLOM 000C,0012,0011,002C ion<Space>
+
+Shown characters are fetched from HID key map file. Default file is Us.hid.txt.
+Shown characters are only for visualisation, the actual conversion from text file to cfg file is done with hid_modifier&key values. 
+HID key map file can be Unicode and output file is always Unicode. 
+
---
+++ TwiddlerCfg2Text.ps1
@@ -0,0 +1,123 @@
+ param (
+    [string]$InputFilename = $(throw "Input filename is required."),
+    [string]$OutputFilename = $(throw "Output filename is required."),
+    [string]$KeyCodeMapFileName = "Us.hid.txt"
+ )
+
+$ChordMap = @{}
+$ChordMap."000"=@("O")
+$ChordMap."001"=@("R")
+$ChordMap."010"=@("M")
+$ChordMap."100"=@("L")
+
+If (!$(Test-Path $KeyCodeMapFileName)) {
+	Write-Output "HID key file not found!"
+	exit 1;
+}
+
+# Read hid key map 
+$KeyCodeMap =  ((get-content $KeyCodeMapFileName -encoding Unicode ) -Replace "\\" , "\\" ) -Join"`r`n" | ConvertFrom-StringData
+
+#Read config file 
+try {
+    [system.io.stream]$stream = [system.io.File]::OpenRead($InputFilename)
+    try {
+      [byte[]]$InputFile = New-Object byte[] $stream.length
+      [void] $stream.Read($InputFile, 0, $stream.Length);
+      #$filebytes
+    } 
+	finally {
+		  $stream.Close();
+		}
+	}
+catch {
+    Write-Error "Error reading file $fileItem - $_"
+    return
+	}
+   
+Set-Content -Path $OutputFilename -Encoding UTF8 "#ConfigFormatVersion : $($InputFile[0])"
+$ChordMapOffset = $InputFile[1]+256*$InputFile[2]
+Add-Content -Path $OutputFilename -Encoding UTF8  "#ChordMapOffset : $($ChordMapOffset)"
+$MouseChordMapOffset = $InputFile[3]+256*$InputFile[4]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#MouseChordMapOffset : $($MouseChordMapOffset)"
+$StringTableOffset = $InputFile[5]+256*$InputFile[6]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#StringTableOffset : $($StringTableOffset)"
+$MouseModeTime = $InputFile[7]+256*$InputFile[8]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#MouseModeTime : $($MouseModeTime)"
+$MouseJumpTime = $InputFile[9]+256*$InputFile[10]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#MouseJumpTime : $($MouseJumpTime)"
+$NormalMouseStartingSpeed = $InputFile[11]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#NormalMouseStartingSpeed : $($NormalMouseStartingSpeed)"
+$MouseJumpModeStartingSpeed = $InputFile[12]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#MouseJumpModeStartingSpeed : $($MouseJumpModeStartingSpeed)"
+$MouseAccelerationFactor = $InputFile[13]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#MouseAccelerationFactor : $($MouseAccelerationFactor)"
+$DelayOnKeyRepeat = $InputFile[14]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#DelayOnKeyRepeat : $($DelayOnKeyRepeat)"
+$Options = $InputFile[15]
+Add-Content -Path $OutputFilename -Encoding UTF8 "#Options : $($Options)"
+$CrrntKeyOffset = 16
+
+# Read string table 
+$StringMap = @{}
+$StringKeyMap = @{}
+$CrrntString = 0
+$StringTablePointer = $StringTableOffset
+while ( $InputFile[$StringTablePointer] -ne 0 -or $InputFile[$StringTablePointer+1] -ne 0){
+	$EntryLen = $InputFile[$StringTablePointer] + 256*$InputFile[$StringTablePointer+1]
+	$StringLen = ($EntryLen-2)/2
+
+	$String =""
+	$StringKey =""
+	for($i=0; $i -le $StringLen-1; $i++){
+		$Modifier = $InputFile[$StringTablePointer+2+2*$i]
+		$KeyCode  = $InputFile[$StringTablePointer+2+2*$i+1]
+		$ModKey = "$("{0:X2}" -f $Modifier)$("{0:X2}" -f $KeyCode)"
+		if (!$KeyCodeMap.ContainsKey($ModKey)){
+			$KeyCodeChr ="<NULL>" 
+		} else { 
+			$KeyCodeChr = $KeyCodeMap[$ModKey]
+		};
+		$String += $KeyCodeChr
+		$StringKey += "$($ModKey),"
+	}
+	$StringKey = $StringKey.Substring(0,$StringKey.Length-1)
+
+	$StringMap[[Convert]::ToString($CrrntString,10)] = @($String)
+	$StringKeyMap[[Convert]::ToString($CrrntString,10)] = @($StringKey)
+
+	$StringTablePointer += $EntryLen
+	$CrrntString += 1
+}
+
+# Read chords
+while ( $InputFile[$CrrntKeyOffset] -ne 0 -or $InputFile[$CrrntKeyOffset+1] -ne 0 -or $InputFile[$CrrntKeyOffset+2] -ne 0 -or $InputFile[$CrrntKeyOffset+3] -ne 0  ){
+
+$Chord = $InputFile[$CrrntKeyOffset] + 256*$InputFile[$CrrntKeyOffset+1]
+$Modifier = $InputFile[$CrrntKeyOffset+2]
+$KeyCode = $InputFile[$CrrntKeyOffset+3]
+
+$ModKey = "$("{0:X2}" -f $Modifier)$("{0:X2}" -f $KeyCode)"
+
+$ChordBit = $([Convert]::ToString($Chord,2).PadLeft(16,'0'))
+$ChordBit1 = "$($ChordMap[$ChordBit.Substring(12,3)])$($ChordMap[$ChordBit.Substring(8,3)])$($ChordMap[$ChordBit.Substring(4,3)])$($ChordMap[$ChordBit.Substring(0,3)])"
+$ChordBit2S = switch ( $ChordBit.Substring(3,1)){"1" {"S"};default {""}}
+$ChordBit2C = switch ( $ChordBit.Substring(7,1)){"1" {"C"};default {""}}
+$ChordBit2A = switch ( $ChordBit.Substring(11,1)){"1" {"A"};default {""}}
+$ChordBit2N = switch ( $ChordBit.Substring(15,1)){"1" {"N"};default {""}}
+$ChordBit2 = "$($ChordBit2S)$($ChordBit2C)$($ChordBit2A)$($ChordBit2N)"
+$ChordBit2 = switch($ChordBit2 ){"" {"O"};default {$_}}
+
+if( $Modifier -eq 255 ){
+	Add-Content -Path $OutputFilename -Encoding UTF8 "$($ChordBit2.PadLeft(4,' ')) $ChordBit1 $($StringKeyMap[[Convert]::ToString($KeyCode,10)]) $($StringMap[[Convert]::ToString($KeyCode,10)])"
+} else {
+	if (!$KeyCodeMap.ContainsKey($ModKey)){
+		$KeyCodeChr ="<NULL>" 
+	} else { 
+		$KeyCodeChr = $KeyCodeMap[$ModKey]
+	};
+	
+	Add-Content -Path $OutputFilename -Encoding UTF8 "$($ChordBit2.PadLeft(4,' ')) $ChordBit1 $ModKey $KeyCodeChr" 
+	}
+	$CrrntKeyOffset += 4
+}
